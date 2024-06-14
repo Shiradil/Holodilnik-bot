78ba4a0d8addb261b04d20c2ddae636e9a41c474
Shiradil <shirbayev04@mail.ru>
2024-06-14 17:23:33 +0500

    Initial commit

---
+++ cmd/bot/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"log"
+)
+
+func main() {
+	discord, err := discordgo.New("Bot " + "MTI1MTE0MzI2NzE2OTU5OTQ5OA.Gx3xML.l-LkXnDEm1ia2PtPcoOG892-o6-yclDh4gtRw8")
+	if err != nil {
+		log.Fatal("ggwp")
+	}
+
+}
